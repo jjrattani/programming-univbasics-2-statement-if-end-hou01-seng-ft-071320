f65222c8d6d18735f678e823b6a762a8b095fc61
Jessica  Rattani <jjrattani@gmail.com>
2020-06-23 19:26:51 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,13 +1,6 @@
-chocolate bars = 6
-if chocolate bars > 0.
-  puts "Even"
+if TRUE
+  puts "Even!"
 else 
   puts "Odd!"
 end
   
-  chocolate bars = 6
-if chocolate bars > 0+1.
-  puts "Even"
-else 
-  puts "Odd!"
-end
